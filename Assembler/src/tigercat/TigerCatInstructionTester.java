f638a92ed8dc12f784b899b542c7bd55345a01a8
Simon Redman <simon@ergotech.com>
2017-10-16 23:47:36 -0600

    Add tester

---
+++ Assembler/src/tigercat/TigerCatInstructionTester.java
@@ -0,0 +1,34 @@
+package tigercat;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TigerCatInstructionTester
+{
+
+  @Test
+  public void testGetMachineCode()
+  {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testConvertIntToByteArray()
+  {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testGetSize()
+  {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testCreateInstruction()
+  {
+    fail("Not yet implemented");
+  }
+
+}

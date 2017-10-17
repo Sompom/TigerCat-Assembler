91c462444fa40d032d70fb0cc3bed4f5867ca384
Simon Redman <simon@ergotech.com>
2017-10-17 00:35:09 -0600

    Add test cases

--- Assembler/src/tigercat/TigerCatInstructionTester.java
+++ Assembler/src/tigercat/TigerCatInstructionTester.java
@@ -2,33 +2,69 @@ package tigercat;
 
 import static org.junit.Assert.*;
 
+import java.util.HashMap;
+
+import org.junit.Assert;
 import org.junit.Test;
+
+import tigercat.instruction.Instruction;
+import tigercat.instruction.InstructionArgumentCountException;
+import tigercat.instruction.InstructionSyntaxError;
+import tigercat.instruction.InvalidOpcodeException;
+import tigercat.instruction.InvalidRegisterException;
 
 public class TigerCatInstructionTester
 {
 
   @Test
-  public void testGetMachineCode()
+  public void testConvertIntToByteArray1()
   {
-    fail("Not yet implemented");
+    int test = 0x55AA55AA;
+    Byte[] toCheck = Instruction.convertIntToByteArray(test);
+    
+    Assert.assertArrayEquals(new Byte[]{0x55, (byte) 0xAA, 0x55, (byte) 0xAA}, toCheck);
   }
 
   @Test
-  public void testConvertIntToByteArray()
+  public void testConvertIntToByteArray2()
   {
-    fail("Not yet implemented");
+    int test = 0xFEDCBA98;
+    Byte[] toCheck = Instruction.convertIntToByteArray(test);
+    
+    Assert.assertArrayEquals(new Byte[]{(byte) 0xFE, (byte) 0xDC, (byte) 0xBA, (byte) 0x98}, toCheck);
   }
 
   @Test
-  public void testGetSize()
+  public void testCreateInstructionNoMap() throws InstructionArgumentCountException, InvalidOpcodeException, InstructionSyntaxError, InvalidRegisterException
   {
-    fail("Not yet implemented");
+    Instruction toCheck = Instruction.createInstruction("addd %arg1 %arg1 %arg1", null);
   }
 
   @Test
-  public void testCreateInstruction()
+  public void testCreateInstructionWithMap() throws InstructionArgumentCountException, InvalidOpcodeException, InstructionSyntaxError, InvalidRegisterException
   {
-    fail("Not yet implemented");
+    Instruction toCheck = Instruction.createInstruction("addd %arg1 %arg1 %arg1", new HashMap<String, Label>());
+  }
+
+  @Test
+  public void testGetSizeNoMap() throws InstructionArgumentCountException, InvalidOpcodeException, InstructionSyntaxError, InvalidRegisterException
+  {
+    Instruction toCheck = Instruction.createInstruction("addd %arg1 %arg1 %arg1", null);
+    Assert.assertEquals((Integer)2, toCheck.getSize());
+  }
+
+  @Test
+  public void testGetSizeWithMap() throws InstructionArgumentCountException, InvalidOpcodeException, InstructionSyntaxError, InvalidRegisterException
+  {
+    Instruction toCheck = Instruction.createInstruction("addd %arg1 %arg1 %arg1", new HashMap<String, Label>());
+    Assert.assertEquals((Integer)2, toCheck.getSize());
+  }
+
+  @Test
+  public void testGetMachineCodeWithMap() throws InstructionArgumentCountException, InvalidOpcodeException, InstructionSyntaxError, InvalidRegisterException
+  {
+    Instruction toCheck = Instruction.createInstruction("addd %arg1 %arg1 %arg1", new HashMap<String, Label>());
+    Assert.assertArrayEquals(new Byte[]{0x06, (byte) 0x92, 0x00, 0x00}, toCheck.getMachineCode());
   }
 
 }

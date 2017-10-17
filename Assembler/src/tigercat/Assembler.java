9945e013d55b5d3c126d9f89c398eb95ab450c2b
Simon Redman <simon@ergotech.com>
2017-10-16 18:28:27 -0600

    Add Assembler Eclipse project with Assembler class

---
+++ Assembler/src/tigercat/Assembler.java
@@ -0,0 +1,11 @@
+package tigercat;
+
+import java.util.HashMap;
+
+public class Assembler
+{
+  /**
+   * Store a mapping of label names (strings) to address (integers)
+   */
+  protected HashMap<String, Integer> labelRecords;
+}
